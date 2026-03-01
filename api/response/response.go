3d469949c30ae71179b97dd1e48bc71fa1de7b04
Michael Smith <michael@snacksfor.us>
2026-03-01 10:11:04 -0800

    Omit empty JSON properties in response

--- api/response/response.go
+++ api/response/response.go
@@ -2,14 +2,14 @@ package response
 
 // Error represents an error from the API
 type Error struct {
-	Code    string `json:"code"`
-	Message string `json:"message"`
+	Code    string `json:"code,omitempty"`
+	Message string `json:"message,omitempty"`
 }
 
 // Response represents the response from the API
 type Response struct {
-	Success bool   `json:"success"`
-	Error   *Error `json:"error"`
+	Success bool   `json:"success,omitempty"`
+	Error   *Error `json:"error,omitempty"`
 }
 
 func Success() *Response {
